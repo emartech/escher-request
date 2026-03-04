bb133d45a05a262e035ac354a21f513516ab187a
D056966 <steffen.schroeder01@sap.com>
2026-03-04 09:14:19 +0100

    remove `examples` from ignore (copy-paste)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,28 +5,25 @@ import security from 'eslint-plugin-security';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
-export default defineConfig(
-  globalIgnores(['dist/**', 'examples/**', 'eslint.config.mjs']),
-  {
-    extends: [
-      eslint.configs.recommended,
-      tseslint.configs.recommended,
-      chaiFriendly.configs.recommendedFlat,
-      security.configs.recommended,
-    ],
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    plugins: {
-      'no-only-tests': noOnlyTests,
-    },
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-require-imports': 'off',
-      'no-only-tests/no-only-tests': 'error',
+export default defineConfig(globalIgnores(['dist/**', 'eslint.config.mjs']), {
+  extends: [
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    chaiFriendly.configs.recommendedFlat,
+    security.configs.recommended,
+  ],
+  languageOptions: {
+    parserOptions: {
+      projectService: true,
+      tsconfigRootDir: import.meta.dirname,
     },
   },
-);
+  plugins: {
+    'no-only-tests': noOnlyTests,
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-require-imports': 'off',
+    'no-only-tests/no-only-tests': 'error',
+  },
+});
